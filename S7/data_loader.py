7741964322c43541ee5d7fba1d1fe39bd1157dad
Prarthana Bhat <pbhat@progress.com>
2020-03-11 16:03:17 +0530

    session 7

---
+++ S7/data_loader.py
@@ -0,0 +1,28 @@
+import torch
+import torchvision
+from torchvision import datasets, transforms
+import torchvision.transforms as transforms
+
+transform = transforms.Compose(
+    [transforms.ToTensor(),
+     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+def get_train_loader():
+
+    trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                            download=True, transform=transform)
+    train_loader = torch.utils.data.DataLoader(trainset, batch_size=64,
+                                              shuffle=True, num_workers=2)
+    return train_loader
+
+def get_test_loader():
+    testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                           download=True, transform=transform)
+    test_loader = torch.utils.data.DataLoader(testset, batch_size=64,
+                                             shuffle=False, num_workers=2)
+    return test_loader
+
+def get_classes():
+    classes = ('plane', 'car', 'bird', 'cat',
+           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+    return classes
